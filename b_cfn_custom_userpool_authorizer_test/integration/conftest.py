d617463f72a1c7d4f395146172e7751f6ad0dcd1
laimonassutkus <laimonas.sutkus@gmail.com>
2021-09-23 13:22:45 +0300

    Add fixtures.

--- b_cfn_custom_userpool_authorizer_test/integration/conftest.py
+++ b_cfn_custom_userpool_authorizer_test/integration/conftest.py
@@ -1,6 +1,9 @@
+# Import all the fixtures.
+# noinspection PyUnresolvedReferences
+from b_cfn_custom_userpool_authorizer_test.integration.fixtures import *
+
 from b_cfn_custom_userpool_authorizer_test.integration.infra_create import inf_create
 from b_cfn_custom_userpool_authorizer_test.integration.infra_destroy import inf_destroy
-from b_cfn_custom_userpool_authorizer_test.integration.manager import MANAGER
 
 
 def pytest_sessionstart(session):
